a027007251180fdbb7de30babe466aeb2f072f23
OmkarTerbhai <omkarterbhai@gmail.com>
2024-01-29 19:47:19 +0530

    Updated unit tests.

--- src/test/java/com/omkar/app/AppTest.java
+++ src/test/java/com/omkar/app/AppTest.java
@@ -14,12 +14,23 @@ public class AppTest
      */
     @Test
     public void shouldAnswerWithTrue() throws Exception {
-        QueryBuilder q = QueryBuilder.getBuilder().
-                select("name", "age")
-                .from("Employees")
+        QueryBuilder q = QueryBuilder.getBuilder()
+                .where("salary < 40000")
+                .select("name", "age")
+                .from("MyTable")
+                .orderBy("age")
+                .leftJoin("Department")
+                .on("deptId")
+                .limit(3)
                 .build();
 
-        assertEquals("Basic Query Generated", "SELECT name, age FROM Employees", q.getQuery());
+        assertEquals("Basic Query Generated", "SELECT name, age " +
+                "FROM MyTable " +
+                "WHERE salary < 40000 " +
+                "LEFT JOIN Department " +
+                "ON  MyTable.deptId = Department.deptId " +
+                "ORDER BY age " +
+                "LIMIT 3;", q.getQuery());
     }
 
     @Test
